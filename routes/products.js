34b47471b30399b77828602df972aaa2adbb0c01
Daniel Rozov <danielrozov@gmail.com>
2020-03-17 11:40:58 +0200

    Modified routes/products. Created get and post endpoints

--- routes/products.js
+++ routes/products.js
@@ -4,13 +4,14 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const products = [
-  { name: "product1" },
-  { name: "product2" },
-  { name: "product3" }
-];
+// const products = [
+//   { name: "product1" },
+//   { name: "product2" },
+//   { name: "product3" }
+// ];
 
-router.get('/', (req, res) => {
+router.get('/', async  (req, res) => {
+  const products = await Product.find().sort('name');
   res.send(products);
 });
 

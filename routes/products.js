3be8002d3af8ec5c167332b01138361e715c3e78
Daniel Rozov <danielrozov@gmail.com>
2020-03-17 10:19:36 +0200

    Modified files: package.json. Deleted @babel/core.

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,27 @@
+const { Product, validate } = require('../models/product');
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const router = express.Router();
 
-const products = [
-  { name: "product1" },
-  { name: "product2" },
-  { name: "product3" }
-];
+// const products = [
+//   { name: "product1" },
+//   { name: "product2" },
+//   { name: "product3" }
+// ];
 
 router.get('/', (req, res) => {
   res.send(products);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const product = new Product({ name: req.body.name });
+  await product.save();
+
+  res.send(product);
 });
 
 module.exports = router;

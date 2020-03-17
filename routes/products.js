f8bd53344fb1fd647e7283fc637b851e8e94b79f
Daniel Rozov <danielrozov@gmail.com>
2020-03-17 13:43:53 +0200

    Modified  routes/products.js. Added get/:id endpoint

--- routes/products.js
+++ routes/products.js
@@ -32,9 +32,25 @@ router.put('/:id', async (req, res) => {
   const product = await Product.findByIdAndUpdate(req.params.id, { name: req.body.name },
     { new: true });
 
-    if(!product) return res.status(404).send('The product with the given ID was not found.');
+  if (!product) return res.status(404).send('The product with the given ID was not found.');
 
-    res.send(product);
-  });
+  res.send(product);
+});
+
+router.delete('/:id', async (req, res) => {
+  const product = await Product.findByIdAndRemove(req.params.id);
+
+  if(!product) return res.status(404).send('The product with the given ID was not found.');
+
+  res.send(product);
+} );
+
+router.get('/:id', async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if(!product) return res.status(404).send('The product with the given ID was not found');
+
+  res.send(product);
+});
 
 module.exports = router;

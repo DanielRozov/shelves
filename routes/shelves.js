2c06e63f773ad25415d8c310236a5ab29f453cf1
Daniel Rozov <danielrozov@gmail.com>
2020-03-25 18:24:00 +0200

    Refactored shelves.js. Added try ctach blocs.

--- routes/shelves.js
+++ routes/shelves.js
@@ -1,109 +1,56 @@
 import express from 'express';
 import { Category } from '../models/category';
-import { Item } from '../models/item';
-import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  let categories;
+router.get('/', async (req, res) => {
   try {
-    categories = await Category.find();
-    if (!categories) {
-      return res.status(404).send('Categories was not found');
-    }
+    const categories = await Category.find();
     res.send(categories);
-  } catch (e) {
-    next(e);
+  } catch (ex) {
+    return res.status(500).send('Something failed', ex);
   }
 });
 
-router.get('/:categoryName', async (req, res, next) => {
-  const categoryName = req.params.categoryName;
-  validateCategoryName(categoryName);
+router.get('/:categoryName', async (req, res) => {
+  const { categoryName } = req.params;
+
+  console.log(categoryName);
+  if (!categoryName) {
+    return res.status(404).send(`The given category:  ${categoryName} does not exist.`);
+  }
 
   let items = [];
   try {
-    const agg = await Category
-      .aggregate([
-        { $match: { name: categoryName } }
-      ]);
+    const category = await Category.find({ name: categoryName });
 
-    agg.forEach((value) => {
+    category.forEach((value) => {
       items.push(value.item.name);
     });
 
-    if (typeof items == 'undefined' || items.length == 0) {
-      return res
-        .status(404)
-        .send(`This shelve is empty or not exist`);
-    }
     res.send(items);
   } catch (e) {
-    next(e);
+    return res.status(404).send('This shelve is empty or does not exist', e);
   }
 });
 
-router.get('/:categoryName/:itemName', async (req, res, next) => {
-  let categoryName = req.params.categoryName;
-  validateCategoryName(categoryName);
-
-  let itemName = req.params.itemName;
-  validateItemName(itemName);
-
+router.get('/:categoryName/:itemName', async (req, res) => {
+  const { categoryName, itemName } = req.params;
+ 
   let items = [];
   try {
-    const agg = await Category
-      .aggregate([
-        { $match: { name: categoryName } }
-      ]);
+    const category = await Category.find({ name: categoryName });
 
-    for (const cat of agg) {
-      if (cat.item.name == itemName) {
-        items.push(cat.item.name);
+    for (const product of category) {
+      if (product.item.name === itemName) {
+        items.push(product.item.name);
       }
     }
-    if (typeof items == 'undefined' || items.length == 0) {
-      return res
-        .status(404)
-        .send(`The given item: ${itemName} does not belong to category: ${categoryName}`);
-    }
+
     res.send(items);
   } catch (e) {
-    next(e);
+    return res.status(404).send('The given item or category does not exist.', e);
   }
 });
 
-function validateCategoryName(name) {
-  const regex = '/^(food|hygiene)$/ig';
-  const isCategory = name.match(regex);
-  if (isCategory) {
-    return res.status(400).send(`Category: ${name} does not exist`);
-  }
-}
-
-function validateItemName(name) {
-  const regex = '/^(fruits|meat|fish|drinks|bread|tampon|deodorant|antiprespirant|aftershave|shampoo)$/ig';
-  const isItem = (name.match(regex));
-  if (isItem) {
-    return res.status(400).send(`Item: ${name} does not exist`);
-  }
-}
-
-// Helper method. 
-// Searches for all categories: food and hygiene for a given categoryName 
-// and returns array of categories
-async function findCategoryByCategoryName(categoryName) {
-  const names = await Category
-    .aggregate([{ $match: { 'name': { $regex: /^(food|hygiene)$/ } } }]);
-
-  let category;
-  names.forEach((c) => {
-    if (c.item.name == categoryName) {
-      category = c;
-    }
-  });
-
-  return category;
-}
 export default router;
